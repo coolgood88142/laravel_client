92cb2760e55a26ae0555a099cacc601741a51971
Kai <coolgood88142@gmail.com>
2022-03-21 10:54:43 +0800

    :art: 更新程式碼

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,10 +25,4 @@ class HomeController extends Controller
     {
         return view('home');
     }
-
-    public function test()
-    {
-        dd('test');
-        // return view('home');
-    }
 }
